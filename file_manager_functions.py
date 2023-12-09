cf7698ee70d06881407b81436683aa315f9e99db
aizikov <mikslakesha@gmail.com>
2023-12-09 18:50:37 +0300

    коменты

--- file_manager_functions.py
+++ file_manager_functions.py
@@ -1,61 +1,85 @@
 import os
 import fnmatch
 
+# Устанавливаем директорию, в которой будем работать
 directory = './docs/'
 
 class FileManagerFunctions:
     @staticmethod
     def get_files_list(mask, sort):
+        # Получаем список файлов в директории с помощью list comprehension и фильтруем их по расширению
         files = [f for f in os.listdir(directory) if f.endswith('.txt')]
+        # Применяем маску к файлам с помощью fnmatch
         filtered_files = [file for file in files if fnmatch.fnmatch(file, mask)]
 
+        # Сортируем файлы в зависимости от выбранного метода сортировки
         if sort == 'Extension':
-            filtered_files.sort(key=lambda x: x.split('.')[-1])  # Sort the files by extension
+            filtered_files.sort(key=lambda x: x.split('.')[-1])
+            # Сортируем файлы по расширению
         elif sort == 'Modification Time':
-            filtered_files.sort(key=lambda x: os.path.getmtime(os.path.join(directory, x)))  # Sort the files by modification time
+            # Сортируем файлы по времени последнего изменения
+            filtered_files.sort(key=lambda x: os.path.getmtime(os.path.join(directory, x)))
         else:
-            filtered_files.sort()  # Sort the files by name
+            # Сортируем файлы по имени
+            filtered_files.sort()
         return filtered_files
 
     @staticmethod
     def delete_file(file_name):
+        # Получаем полный путь к файлу
         file_path = directory + file_name
+        # Удаляем файл
         os.remove(file_path)
 
-    # Добавьте методы для создания, переименования и редактирования файлов
+    # Добавляем методы для создания, переименования и редактирования файлов
 
     @staticmethod
     def create_empty_file(file_name):
+        # Формируем полный путь к новому файлу
         file_path = directory + file_name + '.txt'
         with open(file_path, 'w'):
+            # Создаем новый файл
             pass
 
     @staticmethod
     def rename_file(file_name, new_name):
+        # Получаем полный путь к старому файлу
         old_file_path = directory + file_name
+        # Формируем полный путь к новому имени файла
         new_file_path = directory + new_name
+        # Переименовываем файл
         os.rename(old_file_path, new_file_path)
 
     @staticmethod
     def get_file_content(file_name):
+        # Получаем полный путь к файлу
         file_path = directory + file_name
         try:
+            # Открываем файл для чтения
             with open(file_path, 'r') as file:
+                # Читаем содержимое файла
                 content = file.read()
                 return content
         except FileNotFoundError:
+            # Если файл не найден, возвращаем пустую строку
             return ''
 
     @staticmethod
     def edit_file(file_name, new_content):
+        # Получаем полный путь к файлу
         file_path = directory + file_name
         try:
+            # Открываем файл для записи
             with open(file_path, 'w') as file:
+                # Записываем новое содержимое файла
                 file.write(new_content)
         except FileNotFoundError:
+            # Если файл не найден, возвращаем None
             return None
     
     @staticmethod
     def get_file_path(file_name):
+        # Получаем полный путь к файлу
         file_path = directory + file_name
+        # Возвращаем полный путь к файлу
         return file_path
--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from PyQt5.QtWidgets import QApplication
 from file_manager_ui import FileManagerUI
 
 if __name__ == '__main__':
+    # Создаем экземпляр приложения
     app = QApplication(sys.argv)
+    # Создаем экземпляр интерфейса файлового менеджера
     file_manager = FileManagerUI()
+    # Отображаем интерфейс файлового менеджера
     file_manager.show()
+    # Запускаем цикл обработки событий приложения
     sys.exit(app.exec_())
